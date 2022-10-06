21a6c3888625eb669563c4b898d1f5c46466276c
siamigo <joakimsa@gmail.com>
2022-10-06 14:54:46 +0200

    Don't you worry about it

--- functions.py
+++ functions.py
@@ -17,6 +17,10 @@ def sensorError(sensor_raw):
         sensor_error[i]=np.sum(np.subtract(sensor_raw[i], realNumber))/len(sensor_raw[i])
     return sensor_error
 
+def removeSensorError(sensor_raw, sensor_error):
+    sensor_value=sensor_raw-sensor_error
+    return sensor_value
+
 # Function taken from Implementation of Kalman Filter with Python Language  by Mohamed LAARAIEDH
 def kalmanFilter(X, P, A, Q, B, U):
     X = np.dot(A, X) + np.dot(B, U)
@@ -25,4 +29,5 @@ def kalmanFilter(X, P, A, Q, B, U):
 
 acc_raw=readFile('accCalib.txt')
 acc_error = sensorError(acc_raw)
-print(acc_error)
+sensor=removeSensorError([[0.01], [-0.01], [1.05]], acc_error)
+print(sensor)
