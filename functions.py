16bb30b4d86d698de8f3ad13fe3f2dbcf24b815c
siamigo <joakimsa@gmail.com>
2022-10-11 17:19:25 +0200

    It's a begining

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
-from numpy import array, average, loadtxt, cov, var, pi
-
+from numpy import average, loadtxt, var, dot, transpose, pi
 # Should be able to make X-D amount of uknown data, and place it in the same dataset array.
 def readFile(filename):
     data_raw = loadtxt(filename, delimiter=',', skiprows=0, dtype=float)
@@ -11,16 +10,27 @@ def sensorError(sensor_raw):
 def getTotalAcceleration(acc_raw):
     acc_x, acc_y, acc_z = acc_raw
     return (acc_x*acc_x+acc_y*acc_y+acc_z*acc_z)**0.5
-
-def getvelocity(ang, dt):
+# Calculates the angular velocity from calibration data
+def calVelocity(ang, dt):
     return [(ang[i] - ang[i-1])/dt * 0.004 for i in range(1, len(ang))]
-# Function taken from Implementation of Kalman Filter with Python Language  by Mohamed LAARAIEDH
+# Give the covariance matrix
 def getCovMatrix(dist, vel ,acc):
-    return [[var(dist), cov(dist,vel), cov(dist,acc)], [cov(vel,dist), var(vel), cov(vel,acc)], [cov(acc,dist), cov(acc,vel), var(acc)]]
+    dist_v, vel_v, acc_v = var(dist), var(vel), var(acc)
+    return [[dist_v, dist_v*vel_v, dist_v*acc_v], [vel_v*dist_v, vel_v, vel_v*acc_v], [acc_v*dist_v, acc_v*vel_v, acc_v]]
+#Gives the variance matrix
+def getVarMatrix(dist, vel, acc):
+    return[[var(dist)], [var(vel)],[var(acc)]]
+# Function taken from Implementation of Kalman Filter with Python Language  by Mohamed LAARAIEDH
+def predictions(X, P, A, Q):
+    X = dot(A, X) #+ dot(B, U)
+    P = dot(A, dot(P, transpose(A))) + Q
+    return(X,P)
 
-v_raw=getvelocity(angle)
+dis_calib, ang_raw, acc_raw=readFile('disAngCalib.txt')
 acc_raw = readFile('accCalib.txt')
-acc_error=sensorError(acc_raw)
-acc_corr = getTotalAcceleration(acc_raw)
+acc_calib = getTotalAcceleration(acc_raw)
+vel_calib=calVelocity(ang_raw, 0.0031)
 
-print(acc_corr)
+R = getCovMatrix(dis_calib, vel_calib, acc_calib)
+x0=[[average(dis_calib)],[average(vel_calib)], [average(acc_calib)]]
+H=[1,1,1]
--- kalman.py
+++ kalman.py
@@ -1,43 +1,18 @@
-#from statistics import variance, covariance
-from numpy import *
-def readFile(filename):
-    dataSet1=[]
-    dataSet2=[]
-    dataset3=[]
+from numpy import array
+from functions import *
 
-    with open(filename, 'r') as dataFile:
-        headerLine = dataFile.readline()
-        headerLine = headerLine.strip("\n")
-        for line in dataFile:
-            line = line.strip("\n")
-            lineList = line.split(",")
-            dataSet1.append(float(lineList[0]))
-            dataSet2.append(float(lineList[1]))
-            dataset3.append(float(lineList[2]))
-    return dataSet1, dataSet2, dataset3
+dis_calib, ang_raw, acc_raw=readFile('disAngCalib.txt')
+acc_raw = readFile('accCalib.txt')
+acc_calib = getTotalAcceleration(acc_raw)
+vel_calib=calVelocity(ang_raw, 0.0031)
 
-def calibrateGyrometer(gyr_x, gyr_y, gyr_z):
-    gyr_x_error=gyr_x/131/len(gyr_x)
-    gyr_y_error=gyr_y/131/len(gyr_y)
-    gyr_z_error=gyr_z/131/len(gyr_z)
-    return gyr_x_error, gyr_y_error, gyr_z_error
+dt=0.032
+R = getCovMatrix(dis_calib, vel_calib, acc_calib)
+x0=[[average(dis_calib)],[average(vel_calib)], [average(acc_calib)]]
+H=[1,1,1]
+A = [[1, dt, 0.5*(dt**2)], [0.0, 1., dt], [0.0, 0.0, 1]] 
+Q = array([[(dt**6)/36., (dt**5)/12., (dt**4)/6.], [(dt**5)/12., (dt**4)/6., (dt**3)/2.], [(dt**4)/6., (dt**3)/2., dt**2]])*array(getVarMatrix(dis_calib, vel_calib, acc_calib))
+X,P =predictions(x0, R, A, Q)
 
-def calibrateAccelerometer(acc_x, acc_y, acc_z):
-    acc_x_error=(acc_x/16384.)/float(len(acc_x))
-    acc_y_error=(acc_y/16384.)/float(len(acc_y))
-    acc_z_error=(acc_z/16384.)/float(len(acc_z))
-    return acc_x_error, acc_y_error, acc_z_error
-
-# Function taken from Implementation of Kalman Filter with Python Language  by Mohamed LAARAIEDH
-def kalmanFilter(X, P, A, Q, B, U):
-    X = dot(A, X) #+ dot(B, U)
-    P = dot(A, dot(P, A.T)) + Q
-    return(X,P)
-
-dt = 0.5 #Give it an actually value
-variance2 = 15 #Give it an actually value
-
-X = array([[0.0], [0.0], [0.0]])
-P = diag((0.0, 0.0, 0.0))
-A = array([[1, dt, 0.5*(dt**2)], [0.0, 1., dt], [0.0, 0.0, 1]]) 
-Q = array[[(dt**6)/36, (dt**5)/12, (dt**4)/6], [(dt**5)/12, (dt**4)/6, (dt**3)/2], [(dt**4)/6, (dt**3)/2, dt**2]]*variance2
+print(Q)
+print(f"X={X},P={P}")
