7740cfa3c31dce647ea0bd5d464865a1f140f3a2
siamigo <joakimsa@gmail.com>
2022-10-11 21:51:08 +0200

    Something

--- functions.py
+++ functions.py
@@ -1,36 +1,39 @@
-from numpy import average, loadtxt, var, dot, transpose, pi
+from numpy import average, loadtxt, var, dot, transpose, zeros, shape, array, around, subtract, pi
 # Should be able to make X-D amount of uknown data, and place it in the same dataset array.
 def readFile(filename):
     data_raw = loadtxt(filename, delimiter=',', skiprows=0, dtype=float)
-    return [data_raw[:,i] for i, _ in enumerate(data_raw[0])]
+    return array([data_raw[:,i] for i, _ in enumerate(data_raw[0])])
 # Calculates error from the closes integer.
 def sensorError(sensor_raw):
-    return [average(sensor_raw[index])-int(average(sensor_raw[index])) for index, _ in enumerate(sensor_raw)]
+    return array([average(sensor_raw[index])-0 for index, _ in enumerate(sensor_raw)])
 # Gives total acceleration from raw data.
 def getTotalAcceleration(acc_raw):
     acc_x, acc_y, acc_z = acc_raw
-    return (acc_x*acc_x+acc_y*acc_y+acc_z*acc_z)**0.5
+    return acc_x*acc_x+acc_y*acc_y+acc_z*acc_z**(0.5) #subtract((acc_x*acc_x+acc_y*acc_y+acc_z*acc_z)**(0.5),sum(acc_error))
 # Calculates the angular velocity from calibration data
 def calVelocity(ang, dt):
-    return [(ang[i] - ang[i-1])/dt * 0.004 for i in range(1, len(ang))]
+    return array([(ang[i] - ang[i-1])/dt * 0.004 for i in range(1, len(ang))])
 # Give the covariance matrix
 def getCovMatrix(dist, vel ,acc):
     dist_v, vel_v, acc_v = var(dist), var(vel), var(acc)
-    return [[dist_v, dist_v*vel_v, dist_v*acc_v], [vel_v*dist_v, vel_v, vel_v*acc_v], [acc_v*dist_v, acc_v*vel_v, acc_v]]
+    return array([[dist_v, dist_v*vel_v, dist_v*acc_v], [vel_v*dist_v, vel_v, vel_v*acc_v], [acc_v*dist_v, acc_v*vel_v, acc_v]])
 #Gives the variance matrix
 def getVarMatrix(dist, vel, acc):
-    return[[var(dist)], [var(vel)],[var(acc)]]
+    return array([[var(dist)], [var(vel)],[var(acc)]])
 # Function taken from Implementation of Kalman Filter with Python Language  by Mohamed LAARAIEDH
 def predictions(X, P, A, Q):
     X = dot(A, X) #+ dot(B, U)
     P = dot(A, dot(P, transpose(A))) + Q
     return(X,P)
 
-dis_calib, ang_raw, acc_raw=readFile('disAngCalib.txt')
-acc_raw = readFile('accCalib.txt')
-acc_calib = getTotalAcceleration(acc_raw)
-vel_calib=calVelocity(ang_raw, 0.0031)
-
-R = getCovMatrix(dis_calib, vel_calib, acc_calib)
-x0=[[average(dis_calib)],[average(vel_calib)], [average(acc_calib)]]
-H=[1,1,1]
+def kalman_gain(P_kd, Hd, Rd):
+    num = dot(P_kd, transpose(Hd))
+    den = dot(dot(Hd, P_kd), transpose(Hd)) + Rd
+    kk = zeros(shape(P_kd))
+    for i in range(len(P_kd)):
+        for j in range(len(P_kd)):
+            if (num[i, j] or den[i, j]) == 0:
+                kk[i, j] = 0
+            else:
+                kk[i, j] = num[i, j] / den[i, j]
+    return kk
--- kalman.py
+++ kalman.py
@@ -1,18 +1,20 @@
-from numpy import array
+from numpy import array, around, identity
 from functions import *
 
+# All the calibration sensor data
 dis_calib, ang_raw, acc_raw=readFile('disAngCalib.txt')
-acc_raw = readFile('accCalib.txt')
+acc_raw = readFile('accCalib.txt')*9.81
+acc_error = sensorError(acc_raw)
 acc_calib = getTotalAcceleration(acc_raw)
 vel_calib=calVelocity(ang_raw, 0.0031)
 
 dt=0.032
 R = getCovMatrix(dis_calib, vel_calib, acc_calib)
-x0=[[average(dis_calib)],[average(vel_calib)], [average(acc_calib)]]
-H=[1,1,1]
+x0=around(array([[average(dis_calib)],[average(vel_calib)], [0]]),2)
+H=identity(len(R))
 A = [[1, dt, 0.5*(dt**2)], [0.0, 1., dt], [0.0, 0.0, 1]] 
-Q = array([[(dt**6)/36., (dt**5)/12., (dt**4)/6.], [(dt**5)/12., (dt**4)/6., (dt**3)/2.], [(dt**4)/6., (dt**3)/2., dt**2]])*array(getVarMatrix(dis_calib, vel_calib, acc_calib))
+Q = array([[(dt**6)/36., (dt**5)/12., (dt**4)/6.], [(dt**5)/12., (dt**4)/6., (dt**3)/2.], [(dt**4)/6., (dt**3)/2., dt**2]])*getVarMatrix(dis_calib, vel_calib, acc_calib)
 X,P =predictions(x0, R, A, Q)
 
-print(Q)
-print(f"X={X},P={P}")
+print(acc_error)
+#print(f"X={X},P={P}")
